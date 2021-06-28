5b335d6bd6e8bcd919cca9be8a96f76efd4ef8a8
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-06-28 11:36:45 +0100

    HOCS-3290 - Add shutdown logging

--- src/main/java/uk/gov/digital/ho/hocs/templates/TemplatesApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/templates/TemplatesApplication.java
@@ -1,15 +1,24 @@
 package uk.gov.digital.ho.hocs.templates;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PreDestroy;
+
+@Slf4j
 @SpringBootApplication
 public class TemplatesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplatesApplication.class, args);
 
+	}
+
+	@PreDestroy
+	public void stop() {
+		log.info("hocs-templates stopping gracefully");
 	}
 
 }
