9919fa72862997ee62e4e6466e2ffc612935940d
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-08-01 16:23:05 +0100

    Spike of jlink image

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec ~/jre/bin/java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.JarLauncher
+exec ~jre/bin/java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.JarLauncher
