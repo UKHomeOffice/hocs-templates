d2b74558b0bfa1102c5beb2099940139f9696931
Vinoth Srinivasan <35428275+vinothon84@users.noreply.github.com>
2024-02-09 09:12:30 +0000

    HOCS-6849:build changes to upgrade spring boot 3.1.5 (#241)
    * HOCS-6849:build changes to upgrade spring boot 3.1.5
    
    * HOCS-6849:Updated test package name and build changes to move switch to if statement as with spring 3.X, httpsStatuscode is no more an enum
    
    * Added exclude strategy to avoid duplicate jar as part of bootJar task
    
    * HOCS-6849: Upgrade wiremock to 3.0.1 to resovle junit failure

--- src/main/java/uk/gov/digital/ho/hocs/templates/TemplatesApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/templates/TemplatesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 
 @Slf4j
 @SpringBootApplication
